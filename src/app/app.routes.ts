17f7a87cf03354b7b00a1bbd603c4682ce91ad75
Frederic <fredericdonfack13@gmail.com>
2024-10-10 16:30:35 +0100

    ajout du style des balises <a> de facon globale

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CandidaturesComponent } from './candidatures/candidatures.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 
 export const routes: Routes = [
   {path: '', redirectTo:'/candidatures', pathMatch:'full' },
-  {path: 'candidatures', component: CandidaturesComponent, title:'Candidature' }
+  {path: 'candidatures', component: CandidaturesComponent, title:'Candidature' },
+  {path: 'formulaires', component: FormulaireComponent, title: 'formulaire'}
 ];
